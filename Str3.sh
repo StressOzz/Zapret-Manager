c63edb870b3397f012acb491fc03e291fc7234b8
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-09 21:49:02 +0500

    Update Str3.sh

--- Str3.sh
+++ Str3.sh
@@ -50,18 +50,8 @@ cat <<'EOF' >> /etc/config/zapret
 --dpi-desync-fake-quic=/opt/zapret/files/fake/quic_initial_www_google_com.bin
 '
 EOF
-# Редактируем /etc/hosts
-echo -e "${GREEN}🔴 ${CYAN}Добавляем домены для ${NC}Youtube${CYAN} и редактируем ${NC}/etc/hosts"
-file="/etc/hosts"
-cat <<'EOF' | grep -Fxv -f "$file" 2>/dev/null >> "$file"
-130.255.77.28 ntc.party
-57.144.222.34 instagram.com www.instagram.com
-173.245.58.219 rutor.info d.rutor.info
-193.46.255.29 rutor.info
-157.240.9.174 instagram.com www.instagram.com
-EOF
-/etc/init.d/dnsmasq restart >/dev/null 2>&1
-# добавление исключения
+# Добавляем исключение и домены Youtube
+echo -e "${GREEN}🔴 ${CYAN}Добавляем домены для ${NC}Youtube"
 file="/opt/zapret/ipset/zapret-hosts-user-exclude.txt"
 rm -f "$file"
 cat <<'EOF' > "$file"
@@ -94,6 +84,17 @@ EOF
 # дкопируем tls_clienthello_t2_ru.bin
 echo -e "${GREEN}🔴 ${CYAN}Копируем ${NC}tls_clienthello_t2_ru.bin${CYAN} на устройство${NC}"
 curl -sLo /opt/zapret/files/fake/tls_clienthello_t2_ru.bin https://github.com/StressOzz/Zapret-Manager/raw/refs/heads/main/tls_clienthello_t2_ru.bin
+# Редактируем /etc/hosts
+echo -e "${GREEN}🔴 ${CYAN}Редактируем ${NC}/etc/hosts"
+file="/etc/hosts"
+cat <<'EOF' | grep -Fxv -f "$file" 2>/dev/null >> "$file"
+130.255.77.28 ntc.party
+57.144.222.34 instagram.com www.instagram.com
+173.245.58.219 rutor.info d.rutor.info
+193.46.255.29 rutor.info
+157.240.9.174 instagram.com www.instagram.com
+EOF
+/etc/init.d/dnsmasq restart >/dev/null 2>&1
 # Применяем конфиг
 echo -e "${GREEN}🔴 ${CYAN}Применяем новую стратегию и настройки${NC}\n"
 chmod +x /opt/zapret/sync_config.sh && /opt/zapret/sync_config.sh && /etc/init.d/zapret restart >/dev/null 2>&1
