2a2e95d28fb27e90f866cdc637ee7f709285e9e6
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-09 22:26:49 +0500

    Update Str2.sh

--- Str2.sh
+++ Str2.sh
@@ -18,7 +18,6 @@ echo -e "${GREEN}🔴 ${CYAN}Меняем стратегию${NC}"
 sed -i "/^[[:space:]]*option NFQWS_OPT '/,\$d" /etc/config/zapret
 # Вставляем новый блок сразу после строки option NFQWS_OPT '
 cat <<'EOF' >> /etc/config/zapret
-#v2
   option NFQWS_OPT '
 --filter-tcp=443
 --hostlist-exclude=/opt/zapret/ipset/zapret-hosts-user-exclude.txt
@@ -39,6 +38,7 @@ cat <<'EOF' >> /etc/config/zapret
 --dpi-desync=fake
 --dpi-desync-repeats=4
 --dpi-desync-fake-quic=/opt/zapret/files/fake/quic_initial_www_google_com.bin
+#v2
 '
 EOF
 # добавление исключения
