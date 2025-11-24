ea633dfce0bd536908b290f98502042be8b00c7d
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-24 17:37:13 +0500

    Update Str3.sh
    Исключения добавлены по умолчанию

--- Str3.sh
+++ Str3.sh
@@ -20,7 +20,7 @@ cat <<EOF >> /etc/config/zapret
 #${version} УДАЛИТЕ ЭТУ СТРОЧКУ, ЕСЛИ ВНОСИТЕ ИЗМЕННЕНИЯ В СТРАТЕГИЮ !!!
 
 --filter-tcp=443
---hostlist-exclude-domains=gstatic.com
+--hostlist-exclude=/opt/zapret/ipset/zapret-hosts-user-exclude.txt
 --dpi-desync=fake,fakeddisorder
 --dpi-desync-split-pos=10,midsld
 --dpi-desync-fake-tls=/opt/zapret/files/fake/tls_clienthello_t2_ru.bin
