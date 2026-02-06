91966176d1b6b0f05670bb2f3905c7bcadc96f16
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-02-06 18:59:19 +0500

    Update Zapret-Manager.sh
    8 пункт ставит  v8

--- Zapret-Manager.sh
+++ Zapret-Manager.sh
@@ -2,7 +2,7 @@
 # ==========================================
 # Zapret on remittor Manager by StressOzz
 # =========================================
-ZAPRET_MANAGER_VERSION="8.6"; ZAPRET_VERSION="72.20260202"; STR_VERSION_AUTOINSTALL="v6"
+ZAPRET_MANAGER_VERSION="8.6"; ZAPRET_VERSION="72.20260202"; STR_VERSION_AUTOINSTALL="v8"
 TEST_HOST="https://rr1---sn-gvnuxaxjvh-jx3z.googlevideo.com"; LAN_IP=$(uci get network.lan.ipaddr 2>/dev/null | cut -d/ -f1)
 GREEN="\033[1;32m"; RED="\033[1;31m"; CYAN="\033[1;36m"; YELLOW="\033[1;33m"; MAGENTA="\033[1;35m"; BLUE="\033[0;34m"; NC="\033[0m"; DGRAY="\033[38;5;244m"
 CONF="/etc/config/zapret"; CUSTOM_DIR="/opt/zapret/init.d/openwrt/custom.d/"; HOSTLIST_FILE="/opt/zapret/ipset/zapret-hosts-user.txt"
