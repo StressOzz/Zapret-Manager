8e7dd17f391c81c8508a80c24adae8bea8a7f054
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-25 19:56:35 +0500

    Update Zapret-Manager.sh

--- Zapret-Manager.sh
+++ Zapret-Manager.sh
@@ -2,8 +2,7 @@
 # ==========================================
 # Zapret on remittor Manager by StressOzz
 # =========================================
-ZAPRET_MANAGER_VERSION="9.2"; STR_VERSION_AUTOINSTALL="v7"
-ZAPRET_VERSION="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/remittor/zapret-openwrt/releases/latest | sed -E 's#.*/tag/v?##')"
+ZAPRET_MANAGER_VERSION="9.2"; STR_VERSION_AUTOINSTALL="v7"; ZAPRET_VERSION="72.20260307"
 TEST_HOST="https://rr1---sn-gvnuxaxjvh-jx3z.googlevideo.com"; LAN_IP=$(uci get network.lan.ipaddr 2>/dev/null | cut -d/ -f1)
 GREEN="\033[1;32m"; RED="\033[1;31m"; CYAN="\033[1;36m"; YELLOW="\033[1;33m"; MAGENTA="\033[1;35m"; BLUE="\033[0;34m"; NC="\033[0m"; DGRAY="\033[38;5;244m"
 CONF="/etc/config/zapret"; CUSTOM_DIR="/opt/zapret/init.d/openwrt/custom.d/"; HOSTLIST_FILE="/opt/zapret/ipset/zapret-hosts-user.txt"
